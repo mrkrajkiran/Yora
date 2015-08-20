b680d9944072bdf06256428105089425e82cc452
Marius Bareiša <m.bareisa@gmail.com>
2015-08-20 22:41:10 +0300

    log in logic

--- app/src/main/java/com/example/mbareisa/yora/activities/LoginNarrowActivity.java
+++ app/src/main/java/com/example/mbareisa/yora/activities/LoginNarrowActivity.java
@@ -3,12 +3,19 @@ package com.example.mbareisa.yora.activities;
 import android.os.Bundle;
 
 import com.example.mbareisa.yora.R;
+import com.example.mbareisa.yora.fragments.LoginFragment;
 
-public class LoginNarrowActivity extends BaseActivity{
+public class LoginNarrowActivity extends BaseActivity implements LoginFragment.Callbacks{
     @Override
     protected void onCreate(Bundle savedState){
         super.onCreate(savedState);
 
         setContentView(R.layout.activity_login_narrow);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        setResult(RESULT_OK);
+        finish();
     }
 }
--- app/src/main/java/com/example/mbareisa/yora/fragments/LoginFragment.java
+++ app/src/main/java/com/example/mbareisa/yora/fragments/LoginFragment.java
@@ -1,17 +1,49 @@
 package com.example.mbareisa.yora.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.mbareisa.yora.R;
 
-public class LoginFragment extends BaseFragment {
+public class LoginFragment extends BaseFragment implements View.OnClickListener {
+    private Button loginButton;
+    private Callbacks callbacks;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedState){
         View view = inflater.inflate(R.layout.fragment_login, root, false);
 
+        loginButton = (Button) view.findViewById(R.id.fragment_login_loginButton);
+        loginButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == loginButton){
+            application.getAuth().getUser().setIsLoggedIn(true);
+            callbacks.onLoggedIn();
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+        callbacks = (Callbacks) activity;
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        callbacks = null;
+    }
+
+    public interface Callbacks{
+        void onLoggedIn();
     }
 }
