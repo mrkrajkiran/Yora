ac62fa4a1b8c8de181e3fbf248561abc8bbade6e
Marius Bareiša <m.bareisa@gmail.com>
2015-09-05 12:34:29 +0300

    Toolbar initialized

--- app/src/main/java/com/example/mbareisa/yora/activities/BaseActivity.java
+++ app/src/main/java/com/example/mbareisa/yora/activities/BaseActivity.java
@@ -1,17 +1,33 @@
 package com.example.mbareisa.yora.activities;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.mbareisa.yora.R;
 import com.example.mbareisa.yora.infrastructure.YoraApplication;
 
 public abstract class BaseActivity extends AppCompatActivity {
     protected YoraApplication application;
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedState){
         super.onCreate(savedState);
 
         application = (YoraApplication) getApplication();
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResId){
+        super.setContentView(layoutResId);
+
+        toolbar = (Toolbar) findViewById(R.id.include_toolbar);
+
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+        }
     }
 
 }
--- app/src/main/java/com/example/mbareisa/yora/activities/MainActivity.java
+++ app/src/main/java/com/example/mbareisa/yora/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mbareisa.yora.activities;
 
 import android.os.Bundle;
+import com.example.mbareisa.yora.R;
 
 public class MainActivity extends BaseAuthenticatedActivity {
     @Override
     protected void onYoraCreate(Bundle savedInstance) {
-
+        setContentView(R.layout.activity_main);
+        assert getSupportActionBar() != null; //this could cause some problems in the future.
+        getSupportActionBar().setTitle("Inbox");
     }
 }
