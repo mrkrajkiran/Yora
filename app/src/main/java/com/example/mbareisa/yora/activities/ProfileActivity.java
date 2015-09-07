44afdb7cf4f529940cbdeaa7685dfa4834a61d9a
Marius Bareiša <m.bareisa@gmail.com>
2015-09-07 20:52:39 +0300

    Profile Activity >>> taking a picture.

--- app/src/main/java/com/example/mbareisa/yora/activities/ProfileActivity.java
+++ app/src/main/java/com/example/mbareisa/yora/activities/ProfileActivity.java
@@ -1,18 +1,34 @@
 package com.example.mbareisa.yora.activities;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
 import com.example.mbareisa.yora.R;
 import com.example.mbareisa.yora.views.MainNavDrawer;
+import com.soundcloud.android.crop.Crop;
 
-public class ProfileActivity extends BaseAuthenticatedActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileActivity extends BaseAuthenticatedActivity implements View.OnClickListener {
+    private static final int REQUEST_SELECT_IMAGE = 100;
+    private ImageView avatarView;
+    private View avatarProgressFrame;
+    private File tempOutputFile;
+
     @Override
     protected void onYoraCreate(Bundle savedInstance) {
         setContentView(R.layout.activity_profile);
         setNavDrawer(new MainNavDrawer(this));
 
+        //switching between tablet and phone view.
         if (!isTablet){
             View textFields = findViewById(R.id.activity_profile_textFields);
 
@@ -21,6 +37,73 @@ public class ProfileActivity extends BaseAuthenticatedActivity {
             params.removeRule(RelativeLayout.END_OF);
             params.addRule(RelativeLayout.BELOW, R.id.activity_profile_change_avatar);
             textFields.setLayoutParams(params);
+        }
+
+        avatarView = (ImageView) findViewById(R.id.activity_profile_avatar);
+        avatarProgressFrame = findViewById(R.id.activity_profile_avatarProgressFrame);
+        tempOutputFile = new File(getExternalCacheDir(), "temp-image.jpg");
+
+        avatarView.setOnClickListener(this);
+        findViewById(R.id.activity_profile_change_avatar).setOnClickListener(this);
+
+        avatarProgressFrame.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+
+        if (viewId == R.id.activity_profile_change_avatar || viewId == R.id.activity_profile_avatar)
+            changeAvatar();
+    }
+
+    private void changeAvatar() {
+        List<Intent> otherImageCaptureIntents = new ArrayList<>();
+        List<ResolveInfo> otherImageCaptureActivities =
+                getPackageManager()
+                        .queryIntentActivities(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), 0);
+
+        for (ResolveInfo info : otherImageCaptureActivities){
+            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            captureIntent.setClassName(info.activityInfo.packageName, info.activityInfo.name);
+            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempOutputFile));
+            otherImageCaptureIntents.add(captureIntent);
+        }
+
+        Intent selectImageIntent = new Intent(Intent.ACTION_PICK);
+        selectImageIntent.setType("image/*");
+
+        Intent chooser = Intent.createChooser(selectImageIntent, "Choose Avatar");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                otherImageCaptureIntents
+                        .toArray(new Parcelable[otherImageCaptureActivities.size()]));
+        startActivityForResult(chooser, REQUEST_SELECT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != RESULT_OK){
+            tempOutputFile.delete();
+            return;
+        }
+
+        if(requestCode == REQUEST_SELECT_IMAGE){
+            Uri outputFile;
+            Uri tempFileUri = Uri.fromFile(tempOutputFile);
+
+            if(data != null && (data.getAction() == null ||
+                    !data.getAction().equals(MediaStore.ACTION_IMAGE_CAPTURE)))
+                outputFile = data.getData();
+
+            else
+                outputFile = tempFileUri;
+            //I hope this works, LOL !!!
+            Crop.of(outputFile, tempFileUri).asSquare().start(this);
+            //new Crop(outputFile).asSquare().output(tempFileUri).start(this);
+        } else if(requestCode == Crop.REQUEST_CROP){
+            //todo: Send TempFileUri to server as new avatar
+            avatarView.setImageResource(0);
+            avatarView.setImageURI(Uri.fromFile(tempOutputFile));
         }
     }
 }
